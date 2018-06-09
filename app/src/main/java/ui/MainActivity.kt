e7ae3281634f83e5b9ccdbe797982d9ff4f0c8ce
PravinSonawane <june.pravin@gmail.com>
2018-06-09 13:07:38 -0700

    Fixes #2. Renamed top level package to ui
    1. Renamed top level package
    2. Moved files in test and androidTest sourceSets to ui package

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.merryapps.namingtips
+package ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.merryapps.namingtips.R.layout
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(layout.activity_main)
   }
 }
